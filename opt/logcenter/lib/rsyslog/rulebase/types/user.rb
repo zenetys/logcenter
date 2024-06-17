f528283290938abd1fe81dd12d733478393d6360
Julien Thomas <jthomas@zenetys.com>
2024-06-17 20:05:18 +0200

    MEDIUM: rsyslog: Make user.rb handle two backslashes between domain and user

--- opt/logcenter/lib/rsyslog/rulebase/types/user.rb
+++ opt/logcenter/lib/rsyslog/rulebase/types/user.rb
@@ -5,13 +5,23 @@ version=2
 ## License: MIT (http://opensource.org/licenses/MIT)
 ##
 
+type=@user_backslash1:%[
+    { "type": "char-to", "name": "user.domain", "extradata": "\\" },
+    { "type": "literal", "text": "\\" },
+    { "type": "char-to", "name": "user.name", "extradata": " " }
+]%
+
+type=@user_backslash2:%[
+    { "type": "char-to", "name": "user.domain", "extradata": "\\\\" },
+    { "type": "literal", "text": "\\\\" },
+    { "type": "char-to", "name": "user.name", "extradata": " " }
+]%
+
 type=@user:%{
     "type": "alternative",
     "parser": [
-        [   { "type": "char-to", "name": "user.domain", "extradata": "\\" },
-            { "type": "literal", "text": "\\" },
-            { "type": "char-to", "name": "user.name", "extradata": " " }
-        ],
+        { "type": "@user_backslash2", "name": ".", "priority": 1000 },
+        { "type": "@user_backslash1", "name": ".", "priority": 2000 },
         { "type": "char-to", "name": "user.name", "extradata": " " }
     ] }%
 
